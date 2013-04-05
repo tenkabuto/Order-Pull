4a71996e8a0fb00c3869a71fc032a3cd6bbdd151
Brandon Hall <tenkabuto@gmail.com>
2013-04-05 15:49:01 -0700

    Specified what sort of order I meant

--- order-pull.php
+++ order-pull.php
@@ -5,6 +5,6 @@ Version: 0.1
 Plugin URI: https://github.com/tenkabuto/order-pull
 Author: Brandon Hall
 Author URI: http://brandon.zeroqualms.net
-Description: Outputting only the order info you need so you can get back to working your magic.
+Description: Outputting only the WooCommerce order info you need so you can get back to working your magic.
 */
 ?>
