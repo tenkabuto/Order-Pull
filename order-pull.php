6544190540e39eda6d5c01e2dbfc08a15547e4b7
Brandon Hall <tenkabuto@gmail.com>
2013-04-04 18:10:27 -0700

    wp_version check

--- order-pull.php
+++ order-pull.php
@@ -7,4 +7,11 @@ Author: Brandon Hall
 Author URI: http://brandon.zeroqualms.net
 Description: Outputting only the order info you need so you can get back to working your magic.
 */
+global $wp_version;
+  
+$exit_msg = 'WP Delish requires WordPress 2.8 or newer. <a href="http://codex.wordpress.org/Upgrading_WordPress">Please update!</a>';
+
+if (version_compare($wp_version, "2.8", "<")) {
+  exit($exit_msg);
+}
 ?>
